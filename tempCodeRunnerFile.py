18916b9567ad64d9a938d600d7c311179f1ef649
Avinashkumarmandal <avinashkumarmandal664@gmail.com>
2024-06-05 14:58:29 +0530

    coding_practice_2024-05-02

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,13 @@
 
 def find_sum_of_digits(number):
     sum_of_digits=0
+    for digit in str(number):
+
+        sum_of_digits+=int(digit)
+    
     #Write your logic here
     return sum_of_digits
 
 #Provide different values for number and test your program
-sum_of_digits=find_sum_of_digits(123)
+sum_of_digits=find_sum_of_digits(196)
 print("Sum of digits:",sum_of_digits)
